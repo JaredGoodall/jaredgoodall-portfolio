f720102cc6e3e56f872567db7346b6bedaeffd25
Jared Goodall <jaredgoodall@Jareds-Air.modem>
2024-04-28 19:03:46 +1000

    Progression

--- src/pages/components/Bento.js
+++ src/pages/components/Bento.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { styled } from '@mui/material/styles';
 import { Box, Paper, Grid, Stack, IconButton, Chip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import jaredWhite from '../images/jared-white.png';
 import goobdoobs from '../images/goobdoobs.png';
 import visualiserWhite from '../images/visualiser-white.png';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -13,7 +12,6 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#bfbfbf',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
@@ -26,125 +24,155 @@ function Bento() {
     'JavaScript (ReactJs)',
     'Java',
     'C',
-    'SQL and NoSQL',
+    'SQL + NoSQL',
     'HTML',
     'CSS',
     'R',
-    'Microsoft Excel Associate Certificaiton',
+    'Microsoft Excel Associate Certification',
     'Sandboxing + VMs',
     'Intrustion Detection Systems',
   ]
 
+  const handleGoobExchangeClick = () => {
+    window.location.href = 'https://goobdoobs.com';
+  };
+
+  const handleLinkedIn = () => {
+    window.location.href = 'https://www.linkedin.com/in/jaredgoodall/';
+  };
+
+  const handleInstagram = () => {
+    window.location.href = 'https://www.instagram.com/jared.goodall/';
+  };
+
+  const handleGitHub = () => {
+    window.location.href = 'https://github.com/JaredGoodall';
+  };
+
   return (
     <Box sx={{ p: margin, height: '100vh', background: '#fff' }}>
       <Box className='App' sx={{ minHeight: '100%', width: '100%' }}>
-        <Grid container spacing={2} columns={{ sm: 1, md: 9 }}>
+        <Grid container rowSpacing={4} columnSpacing={2} columns={{ sm: 1, md: 9 }}>
           <Grid item sm={1} md={3} sx={{ height: `calc(100vh - ${margin} - ${margin})` }}>
-            <Item sx={{ height: '100%' }}>
-              <img
-                src={jaredWhite}
-                className="mainLogo invert"
-                alt="Logo for Jared Goodall"
-              />
-              <h1 style={{ marginBottom: 0 }} >Jared Goodall</h1>
-              <Stack 
-                direction="row"
-                spacing={1} 
-                justifyContent="center"
-              >
-                <IconButton>
-                  <LinkedInIcon />
-                </IconButton>
-                <IconButton>
-                  <InstagramIcon />
-                </IconButton>
-                <IconButton>
-                  <GitHubIcon />
-                </IconButton>
+            <Item sx={{ height: '100%' }} >
+              <Stack direction={'column'} textAlign={'center'} alignItems="center" justify="center">
+                <h1 style={{ marginBottom: 0 }} >Jared Goodall</h1>
+                <Stack 
+                  direction="row"
+                  spacing={1} 
+                  justifyContent="center"
+                >
+                  <IconButton>
+                    <LinkedInIcon onClick={handleLinkedIn} />
+                  </IconButton>
+                  <IconButton>
+                    <InstagramIcon onClick={handleInstagram} />
+                  </IconButton>
+                  <IconButton>
+                    <GitHubIcon onClick={handleGitHub} />
+                  </IconButton>
+                </Stack>
+                <h3 style={{ marginBottom: 5, marginTop: 5 }}>
+                  Computer Science & Commerce (Finance) @ UNSW
+                </h3>
+                <p style={{ marginBottom: 10, marginTop: 5 }}>
+                  I am passionate about using technology to drive creative innovation within business and
+                  software. I am deeply motivated to launch my career and immerse myself in the world of
+                  technology to make tangible impacts.
+                </p>
+                <h4 style={{ marginBottom: 15, marginTop: 5 }}>
+                  Techinal Skills
+                </h4>
+                <Grid container spacing={2} justifyContent="center" sx={{ maxWidth: 400 }}>
+                  {skills.map((skill, index) => (
+                    <Grid
+                      item
+                      xs="auto"
+                      key={index}
+                    >
+                      <Chip label={skill} />
+                    </Grid>
+                  ))}
+                </Grid>
               </Stack>
-              <h3>
-                Computer Science & Commerce (Finance) @ UNSW
-              </h3>
-              <p>
-                I am passionate about using technology to drive creative innovation within business and
-                software. I am deeply motivated to launch my career and immerse myself in the world of
-                technology to make tangible impacts.
-              </p>
-              <p>
-                Currently working within the Cyber Security Industry within the Australian Government
-              </p>
-              <h4>
-                Techinal Skills
-              </h4>
-              <Grid container spacing={2} justifyContent="center" sx={{ maxWidth: 400 }}>
-                {skills.map((skill, index) => (
-                  <Grid
-                    item
-                    xs="auto"
-                    key={index}
-                  >
-                    <Chip label={skill} />
-                  </Grid>
-                ))}
-              </Grid>
             </Item>
           </Grid>
 
           <Grid item sm={1} md={6} sx={{ height: '100%', width: '100%' }}>
             <Grid container spacing={2} direction="column">
               <Grid item>
-                <Item>
-                  <img
-                    src={goobdoobs}
-                    className="logo invert"
-                    alt="Logo for the Goob Exchange"
-                  />
-                  <br />
-                  <h2>Goob Exchange</h2>
-                  <p>
-                    A "currency" trading website that allows users to send and trade different pretend currencies.
-                  </p>
+                <Item onClick={handleGoobExchangeClick}>
+                  <Stack direction="row" sx={{ m: 2 }} gap={2}>
+                    <Box>
+                      <img
+                        src={goobdoobs}
+                        className="logo invert"
+                        alt="Logo for the Goob Exchange"
+                      />
+                    </Box>
+                    <Box>
+                      <h2>Goob Exchange</h2>
+                      <p>
+                        A "currency" trading website that allows users to send and trade different pretend currencies.
+                      </p>
+                    </Box>
+                  </Stack>
                 </Item>
               </Grid>
 
               <Grid item>
                 <Item onClick={() => navigate('/visualiser')}>
-                  <img
-                    src={visualiserWhite}
-                    className="logo invert"
-                    alt="Logo for DM Visualiser"
-                  />
-                  <br />
-                  <h2>IG Visualiser</h2>
-                  <p>
-                    Takes a messages JSON file from Instagram and displays some metrics about it.
-                  </p>
+                  <Stack direction="row" sx={{ m: 2 }} gap={2}>
+                    <Box>
+                      <img
+                        src={visualiserWhite}
+                        className="logo invert"
+                        alt="Logo for DM Visualiser"
+                      />
+                    </Box>
+                    <Box>
+                      <h2>IG Visualiser</h2>
+                      <p>
+                        Takes a messages JSON file from Instagram and displays some metrics about it.
+                      </p>
+                    </Box>
+                  </Stack>
                 </Item>
               </Grid>
 
               <Grid item>
                 <Item>
-                  <img
-                    src={visualiserWhite}
-                    className="logo invert"
-                    alt="Logo for DM Visualiser"
-                  />
-                  <br />
-                  <h2>Todo</h2>
-                  <p>To be done</p>
+                <Stack direction="row" sx={{ m: 2 }} gap={2}>
+                  <Box>
+                    <img
+                      src={visualiserWhite}
+                      className="logo invert"
+                      alt="Logo for DM Visualiser"
+                    />
+                  </Box>
+                  <Box>
+                    <h2>Todo</h2>
+                    <p>To be done</p>
+                  </Box>
+                  </Stack>
                 </Item>
               </Grid>
 
               <Grid item>
                 <Item>
-                  <img
-                    src={visualiserWhite}
-                    className="logo invert"
-                    alt="Logo for DM Visualiser"
-                  />
-                  <br />
-                  <h2>Todo</h2>
-                  <p>To be done</p>
+                  <Stack direction="row" sx={{ m: 2 }} gap={2}>
+                    <Box>
+                      <img
+                        src={visualiserWhite}
+                        className="logo invert"
+                        alt="Logo for DM Visualiser"
+                      />
+                    </Box>
+                    <Box>
+                      <h2>Todo</h2>
+                      <p>To be done</p>
+                    </Box>
+                  </Stack>
                 </Item>
               </Grid>
             </Grid>
